1bef1453ab88fbcb2d0a7490479a64d8b804d842
hieucannguyen <hieucanguyen@yahoo.com>
2024-04-28 06:19:09 +0000

    fix woker bugs

--- src/worker.py
+++ src/worker.py
@@ -2,6 +2,8 @@ from jobs import get_job_by_id, update_job_status, q, rd, rdb
 import json
 import logging
 from datetime import datetime
+import time
+import redis
 
 import logging
 import socket
@@ -19,11 +21,9 @@ def do_work(jobid):
         Args:
             jobid (string): specific jobid
     """
-
     # get job
     update_job_status(jobid, 'in progress')
     job = get_job_by_id(jobid)
-    
     date_freq = get_trips_freq_between_dates(job['start_date'], job['end_date'])
     rdb.set(jobid, json.dumps(date_freq)) # input to results database
     update_job_status(jobid, 'complete') # update job database
@@ -36,14 +36,15 @@ def get_trips_freq_between_dates(start_date, end_date):
             start_date (string): starting date
             end_date (string): ending date
     """
-    start = datetime.strptime(start_date, '%Y-%m-%d').date()
-    end = datetime.strptime(end_date, '%Y-%m-%d').date()
+    start = datetime.strptime(start_date, '%m/%d/%Y').date()
+    end = datetime.strptime(end_date, '%m/%d/%Y').date()
+    logging.debug(start, end)
     result = {}
     for key in rd.keys():
         trip = json.loads(rd.get(key))
-        if trip.get('checkout_datetime'): # none value handling
-            checkout_date = trip.get('checkout_datetime')[:10]
-            date = datetime.strptime(checkout_date, '%Y-%m-%d').date()
+        if trip.get('Checkout Datetime'): # none value handling
+            checkout_date = trip.get('Checkout Datetime')[:10]
+            date = datetime.strptime(checkout_date, '%m/%d/%Y').date()
             if date >= start and date <= end:
                 result[checkout_date] = result.get(checkout_date, 0) + 1
     logging.debug(f'length of result: {len(result)}')
@@ -51,4 +52,9 @@ def get_trips_freq_between_dates(start_date, end_date):
         logging.error('job did not find anything, empty result')
     return result
 
+print(rd.info())
+while rd.info()['loading'] == 1:
+    time.sleep(5)
+
 do_work()
+
